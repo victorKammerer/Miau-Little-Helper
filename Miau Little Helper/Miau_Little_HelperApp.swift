68960ca7e5a0cbb74b7019b90be5598b5bc9d46b
Victor Kammerer <vko@cin.ufpe.br>
2023-03-20 10:07:05 -0300

    Project Created

---
+++ Miau Little Helper/Miau_Little_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Miau_Little_HelperApp.swift
+//  Miau Little Helper
+//
+//  Created by vko on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Miau_Little_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
