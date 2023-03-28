21e69099822d146861c41c6afb929b8f180bc79e
Victor Kammerer <vko@cin.ufpe.br>
2023-03-28 10:45:33 -0300

    nina movement added

--- Miau Little Helper/GameScene.swift
+++ Miau Little Helper/GameScene.swift
@@ -8,7 +8,27 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var nina: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
         print("colocou cena")
+        
+        setupNina()
+    }
+    
+    func setupNina() {
+        
+        let textureAnimation = SKAction.animate(with: ninaMovementTexture, timePerFrame: 0.1)
+        let walk = SKAction.repeatForever(textureAnimation)
+        
+        nina = SKSpriteNode(imageNamed: "nina1")
+        nina.size = CGSize(width: 200, height: 150)
+        nina.position = CGPoint(x: 400, y: 300)
+        
+        nina.run(walk)
+        
+        addChild(nina)
+        
     }
 }
