8b6bb07de5bf304248e4994e0f29e04b83de07f5
Rinaldo Jr <107890806+RinaldoJr4@users.noreply.github.com>
2023-04-07 19:58:23 -0300

    renaming views

--- Miau Little Helper/ConfigurationView.swift
+++ Miau Little Helper/ConfigurationView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ConfigurationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Configuration View")
     }
 }
 
